59045ce338e637d7d178e9273d845250f5c3b28e
Setasena Randata <setasenarr@Setasenas-Mac-Pro.local>
2022-07-29 21:42:20 +0700

    Fixing useEffect

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fa242c7a894c9465"; }
+/******/ 		__webpack_require__.h = function() { return "2ecb4dda922bd11b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
